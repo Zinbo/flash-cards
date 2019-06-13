ab497596243d39b8ab97b53c1922561797191bd4
Shane <shanepjennings@gmail.com>
2019-06-13 11:32:00 +0100

    back end start

--- server/server.ts
+++ server/server.ts
@@ -1,7 +1,7 @@
 const express = require('express')
 const path = require('path')
 const bodyParser = require('body-parser')
-import sayHello from './category-controller'
+import sayHello from './src/categories/categoriesController'
 
 const app = express()
 const port = process.env.PORT || 5000
